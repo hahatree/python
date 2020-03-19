171c0b05bd25048e74dc279460889346a42416b0
Mark <mark91002@gmail.com>
2020-03-20 00:38:14 +0800

    --註釋--

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lab433.desktop-sej47js\documents\北科\吳丞\python\django\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
